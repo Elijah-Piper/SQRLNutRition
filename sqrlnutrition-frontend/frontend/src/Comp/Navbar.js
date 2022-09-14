c3b008d4dcba6fbe09fdf755e6ced0653e8d9d09
Elijah Piper <35882341+Elijah-Piper@users.noreply.github.com>
2022-09-13 21:47:35 -0700

    added login and logout; missing auto reloading

--- sqrlnutrition-frontend/frontend/src/Comp/Navbar.js
+++ sqrlnutrition-frontend/frontend/src/Comp/Navbar.js
@@ -1,12 +1,32 @@
 import '../App.css';
 import {Link} from 'react-router-dom';
+import { useEffect } from 'react';
 
 function Navbar() {
 
-const navStyle ={
+  const navStyle ={
     color: "white"
   
-};
+  };
+
+  const clearUser = () => {
+    localStorage.removeItem("signedInUsername");
+  }
+  
+  function LoginLink() {
+    return (
+      <Link style={navStyle} to="/login">
+        <li> Login </li>
+      </Link>
+    );
+  }
+  function LogoutLink() {
+    return (
+      <Link style={navStyle} to="/" onClick={ clearUser() }>
+        <li> Logout </li>
+      </Link>
+    );
+  }
 
   return (
     <nav>
@@ -14,12 +34,14 @@ const navStyle ={
         <Link style={navStyle} to="/">
           <li> Home </li>
         </Link>
-        <Link style={navStyle} to="/login">
-          <li> Login </li>
-        </Link>
         <Link style={navStyle} to="/sqrls">
           <li> Your Table </li>
         </Link>
+        <LoginLink/>
+        <LogoutLink/>
+        <h3 style={navStyle}>
+          { localStorage.getItem("signedInUsername") ? "Hello, " + localStorage.getItem("signedInUsername") : "Please login or create an account" }
+        </h3>
       </ul>
     </nav>
   );
